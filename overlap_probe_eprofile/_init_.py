fbd499eec238e571adfa0129c3409e9b95e3dfef
Osborne, Martin <martin.osborne@metoffice.gov.uk>
2023-07-12 10:25:13 +0100

    re-structuring to make pip install

---
+++ overlap_probe_eprofile/_init_.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+# expose the package version
+from . import version
+
+
+__version__ = version.__version__
+
---
+++ setup.py
@@ -0,0 +1,49 @@
+from os.path import join
+import logging
+import io
+from setuptools import setup, find_packages
+
+package_name = "overlap_probe_eprofile"
+
+
+def version ( ) :
+
+    package_namespace = {}
+
+    try:
+
+        execfile(join(package_name,"version.py"), package_namespace)
+
+    except NameError:
+
+        exec(
+            open(join(package_name,"version.py")).read(),
+            globals(),
+            package_namespace
+        )
+
+    return package_namespace["__version__"]
+
+def long_description():
+
+    try:
+        with io.open("README.md", encoding="utf-8") as readme:
+
+            return readme.read()
+
+    except IOError:
+
+        logging.warning(
+            "No README.md found, package long_description will be empty"
+        )
+
+        return None
+
+setup(
+    name = package_name,
+    version = version(),
+    description = "EPROFILE overlap temperature correction code",
+    long_description = long_description(),
+    url = "https://github.com/martin-obs/OVERLAP_PROBE_EPROFILE",
+    packages = find_packages()
+)
